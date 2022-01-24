70191cbc393f1cf6db6a12195dc76cee4680de51
abdulsamedbozdag <86298352+abdulsamedbozdag@users.noreply.github.com>
2022-01-24 18:57:16 +0300

    3D-ZikZak Oyunu
    Kolektif Akademi Academy Club Training eğitiminde yaptığımız oyun

---
+++ zikzak/Assets/kodlar/küre_hareket.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class küre_hareket : MonoBehaviour
+{
+    Vector3 yön;
+    public float hiz;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yön = Vector3.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if(yön.x==0)
+            {
+                yön = Vector3.left;
+            }
+            else
+            {
+                yön = Vector3.forward;
+            }
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+        Vector3 kure_move = yön * Time.deltaTime * hiz;
+        transform.position += kure_move;
+    }
+}
---
+++ zikzak/Assets/kodlar/yol_olustur.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yol_olustur : MonoBehaviour
+{
+    public GameObject son_yol;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            yol_oluşturma();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void yol_oluşturma()
+    {
+        son_yol = Instantiate(son_yol, son_yol.transform.position + Vector3.forward, son_yol.transform.rotation);
+    }
+}
